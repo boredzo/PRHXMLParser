2ec437e712c7a457d44e4458fc17d6d44d56e543
Peter Hosey <git@boredzo.org>
2012-12-31 13:14:02 -0800

    Implemented test tool.

--- slurpxml/main.m
+++ slurpxml/main.m
@@ -1,7 +1,18 @@
-#import <Foundation/Foundation.h>
+#import "PRHXMLParser.h"
 
-int main(int argc, char **argv) {
+int main(void) {
 	@autoreleasepool {
+		PRHXMLParser *parser = [[PRHXMLParser alloc] init];
+		[parser setElementHandlerForElementPath:@"rectlist" handler:^(NSXMLElement *element,
+			NSDictionary *attributesDict
+		) {
+			NSLog(@"Found rect list with attributes: %@", attributesDict);
+		}];
+		[parser setElementHandlerForElementName:@"rect" handler:^(NSXMLElement *element, NSDictionary *attributesDict) {
+			NSLog(@"Found rect with contents: %@", element.stringValue);
+		}];
+
+		[parser parseData:[[NSFileHandle fileHandleWithStandardInput] readDataToEndOfFile] completionHandler:nil];
 	}
 	return EXIT_SUCCESS;
 }
